7bdd584ee8f8d7f806a95bd1c7437ea66bba5eec
Oussamahamani <oussamahamani2000@gmail.com>
2024-04-30 22:52:14 -0400

    second commit

--- db/conn.js
+++ db/conn.js
@@ -5,14 +5,15 @@ const connectionString = process.env.ATLAS_URI
 const client = new MongoClient(connectionString)
 
 let conn;
-
+let db ={}
 try {
+    console.log(process.env.ATLAS_URI,'link')
    conn = await client.connect()
    console.log('Connected to MongoDB')
 } catch(err) {
     console.log(err)
 }
 
-let db = conn.db("sample_training")
+ db = conn.db("sample_training")
 
 export default db
